9ec72967c33ba1974e8eeb3515de2e4c765ba1f7
Chrisaen <bondadneil@gmail.com>
2024-06-20 22:03:59 +0800

    creating routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/about-me', function () {
+    return view('about');
+})->name('about');
+
+
+Route::get('/skills', function () {
+    return view('skills');
+})->name('skills');
+
+
+Route::get('/hobbies', function () {
+    return view('hobbies');
+})->name('hobbies');
